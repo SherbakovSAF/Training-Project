8b95c278d1aa3559ba8e0cfa077a9e9a79f2b6c4
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-01 20:05:12 +0300

    Добавил основной функционал для отслеживание текста с карточки из инпута

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-
-async function getObject() {
-     let response = await fetch("http://www.mocky.io/v2/5944e07213000038025b6f30")
-     content = await response.json()
-     return content
+let state = {
+     CardNumber: ""
 }
 
-// getObject().then(data => console.log(data))
 
-async function renderHTML(){
-     let info = [];
-     await getObject().then((data) => (info = data));
-     // debugger
-     let cardArray = info.map((e) => `
-     <div>
-     UserID: ${e.userId}
-     ID: ${e.id}
-     title: ${e.title}
-     body: ${e.body}
-     </div>
-     `
 
-);
-     return cardArray.join("")
+function renderCardNumber(e){
+     e.currentTarget.value = e.currentTarget.value.replace(/[^\d]/g,'')
+     const sep = (xs, s) => xs.length ? [xs.slice(0, s), ...sep(xs.slice(s), s)] : []
+     if(+e.key / 1 == +e.key || e.key == "Backspace"){
+          state.CardNumber = e.currentTarget.value
+          if(state.CardNumber.length <= 16){
+               document.querySelector("h1").innerHTML = sep([...state.CardNumber, "0".repeat(16 - state.CardNumber.length)].join(""), 4).join(" ")
+               // + 
+          } else{
+               console.log("Прекрати да")
+          }
+     } 
 }
 
-renderHTML().then(data => document.querySelector("#f").innerHTML = data)
+//     console.log(document.getElementById("mainInput").innerHTML)
+
+
+// async function getObject() {
+//      let response = await fetch("http://www.mocky.io/v2/5944e07213000038025b6f30")
+//      content = await response.json()
+//      return content
+// }
+
+// // getObject().then(data => console.log(data))
+
+// async function renderHTML(){
+//      let info = [];
+//      await getObject().then((data) => (info = data));
+//      // debugger
+//      let cardArray = info.map((e) => `
+//      <div>
+//      UserID: ${e.userId}
+//      ID: ${e.id}
+//      title: ${e.title}
+//      body: ${e.body}
+//      </div>
+//      `
+
+// );
+//      return cardArray.join("")
+// }
+
+// renderHTML().then(data => document.querySelector("#f").innerHTML = data)
 
 
