f575e2d03657a5b40237a663c48d4c0a666e83ff
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-03 18:25:27 +0300

    Исправил баги, касаемые написания имени\Добавил смену цвета карточки при выборе

--- app.js
+++ app.js
@@ -1,58 +1,82 @@
 // Создание заполнения карточки
 let store = {
-     state: {
-          cardNumber: "",
-          cardName: "",
-          colorBank: {
-               sberbank: "#045A38",
-               tinkoff: "#F8d81c",
-               alpha: "#EF3124",
-               vtb: "blue",
-          },
-          paySystem: {
-               visa: "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png",
-               masterCard: "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/mastercard.png",
-               mir: "https://i.postimg.cc/CL8gXXFx/logo-mir.png",
-               eror: "https://i.postimg.cc/5tF3BhL8/eror.png"
-          }
-     },
-     renderPaySystem(){
-          let cardWrap =  document .querySelector(".card__wrap img")
-          switch (this.state.cardNumber[0]) {
-              case "2":
-                  cardWrap.setAttribute("src", this.state.paySystem.mir);
-                  break
-              case "5":
-                  cardWrap.setAttribute("src", this.state.paySystem.masterCard);
-                  break
-              case "4":
-                  cardWrap.setAttribute("src", this.state.paySystem.visa);
-                  break
-              default:
-                  cardWrap.setAttribute("src", this.state.paySystem.eror);
-          }
-     },
-     renderCardNumber(e){
-          e.currentTarget.value = e.currentTarget.value.replace(/[^\d]/g,'')
-          const sep = (xs, s) => xs.length ? [xs.slice(0, s), ...sep(xs.slice(s), s)] : []
-          
-          if(+e.key / 1 == +e.key || e.key == "Backspace"){
-               this.state.cardNumber = e.currentTarget.value
-               if(this.state.cardNumber.length <= 16){
-                    document.getElementById("cardNumber").innerHTML = sep([...this.state.cardNumber, "#".repeat(16 - this.state.cardNumber.length)].join(""), 4).join(" ")
-                    // + 
-               } else{
-                    console.log("Прекрати да")
-               }
-          } 
-          this.renderPaySystem()
-     },
-     renderCardName(e){
-          e.currentTarget.value = e.currentTarget.value.replace(/[^\W]/g,'')
-          this.state.cardName = e.currentTarget.value
-          document.getElementById("cardName").innerHTML = this.state.cardName
+    state: {
+        cardNumber: "",
+        cardName: "",
+        colorBank: {
+            sberbank: "#045A38",
+            tinkoff: "#F8d81c",
+            alpha: "#EF3124",
+            vtb: "blue",
+            default: "rgb(240, 240, 240)",
+        },
+        paySystem: {
+            visa: "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png",
+            masterCard:
+                "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/mastercard.png",
+            mir: "https://i.postimg.cc/59TFvsfM/logo-mir.png",
+            eror: "https://i.postimg.cc/5tF3BhL8/eror.png",
+        },
+    },
+    renderPaySystem() {
+        let cardWrap = document.querySelector(".card__wrap img");
+        switch (this.state.cardNumber[0]) {
+            case "2":
+                cardWrap.setAttribute("src", this.state.paySystem.mir);
+                break;
+            case "5":
+                cardWrap.setAttribute("src", this.state.paySystem.masterCard);
+                break;
+            case "4":
+                cardWrap.setAttribute("src", this.state.paySystem.visa);
+                break;
+            default:
+                cardWrap.setAttribute("src", this.state.paySystem.eror);
+        }
+    },
+    changeColorCard(){
+     let cardWrap = document.querySelector(".card__wrap");
+     
+     switch (this.state.cardNumber.match(/.{1,4}/g)[0].slice(1)) {
+         case "276":
+             cardWrap.style.backgroundColor = this.state.colorBank.sberbank;
+             break;
+         case "200":
+             cardWrap.style.backgroundColor = this.state.colorBank.tinkoff;
+             break;
+         case "203":
+             cardWrap.style.backgroundColor = this.state.colorBank.alpha;
+             break;
+         default:
+             cardWrap.style.backgroundColor = this.state.colorBank.default;
      }
-}
+    },
+
+    renderCardNumber(e) {
+        e.currentTarget.value = e.currentTarget.value.replace(/[^\d]/g, "");
+        const sep = (xs, s) =>
+            xs.length ? [xs.slice(0, s), ...sep(xs.slice(s), s)] : [];
+        if (+e.key / 1 == +e.key || e.key == "Backspace") {
+            this.state.cardNumber = e.currentTarget.value;
+            if (this.state.cardNumber.length <= 16) {
+                document.getElementById("cardNumber").innerHTML = sep(
+                    [...this.state.cardNumber,"#".repeat(16 - this.state.cardNumber.length),].join(""),4).join(" ");
+            }
+        }
+        this.renderPaySystem();
+        
+        this.changeColorCard()
+    },
+    renderCardName(e) {
+        e.currentTarget.value = e.currentTarget.value.replace(/[^A-Za-z\s]+/g, "");
+        this.state.cardName = e.currentTarget.value;
+        if (this.state.cardName) {
+            document.getElementById("cardName").innerHTML = this.state.cardName;
+        } else {
+            document.getElementById("cardName").innerHTML = "Имя Фамилия";
+        }
+    },
+};
 
 // 4 - visa
 // 5 - МК
