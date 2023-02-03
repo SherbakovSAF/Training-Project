76046b9dfe4578c6256748d57dfb76f5f9e9ac20
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-03 17:11:11 +0300

    Добавил измение лого исходя из начала в номере карты

--- app.js
+++ app.js
@@ -2,27 +2,57 @@
 let store = {
      state: {
           cardNumber: "",
+          cardName: "",
           colorBank: {
                sberbank: "#045A38",
                tinkoff: "#F8d81c",
                alpha: "#EF3124",
                vtb: "blue",
+          },
+          paySystem: {
+               visa: "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png",
+               masterCard: "https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/mastercard.png",
+               mir: "https://i.postimg.cc/CL8gXXFx/logo-mir.png",
+               eror: "https://i.postimg.cc/5tF3BhL8/eror.png"
+          }
+     },
+     renderPaySystem(){
+          let cardWrap =  document .querySelector(".card__wrap img")
+          switch (this.state.cardNumber[0]) {
+              case "2":
+                  cardWrap.setAttribute("src", this.state.paySystem.mir);
+                  break
+              case "5":
+                  cardWrap.setAttribute("src", this.state.paySystem.masterCard);
+                  break
+              case "4":
+                  cardWrap.setAttribute("src", this.state.paySystem.visa);
+                  break
+              default:
+                  cardWrap.setAttribute("src", this.state.paySystem.eror);
           }
      },
      renderCardNumber(e){
-          let cardNumber = store.state.cardNumber
           e.currentTarget.value = e.currentTarget.value.replace(/[^\d]/g,'')
           const sep = (xs, s) => xs.length ? [xs.slice(0, s), ...sep(xs.slice(s), s)] : []
+          
           if(+e.key / 1 == +e.key || e.key == "Backspace"){
-               cardNumber = e.currentTarget.value
-               if(cardNumber.length <= 16){
-                    document.getElementById("cardNumber").innerHTML = sep([...cardNumber, "#".repeat(16 - cardNumber.length)].join(""), 4).join(" ")
+               this.state.cardNumber = e.currentTarget.value
+               if(this.state.cardNumber.length <= 16){
+                    document.getElementById("cardNumber").innerHTML = sep([...this.state.cardNumber, "#".repeat(16 - this.state.cardNumber.length)].join(""), 4).join(" ")
                     // + 
                } else{
                     console.log("Прекрати да")
                }
           } 
-}}
+          this.renderPaySystem()
+     },
+     
+}
+
+// 4 - visa
+// 5 - МК
+// 2 - Мир
 
 // Обучение fetch и ассинхроннорсти
 
